a53d40ac646387d142bc58c2cf3442d8bdb1e537
QuangXYZ <Thanhquang.lord@gmail.com>
2022-10-16 19:03:37 +0700

    Add SignUp Activity

--- app/src/main/java/com/example/shopshoes/Activity/LoginActivity.java
+++ app/src/main/java/com/example/shopshoes/Activity/LoginActivity.java
@@ -90,5 +90,12 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        didNotHaveAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
